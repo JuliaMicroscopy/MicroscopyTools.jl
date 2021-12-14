0cf74c8f59afe61d7b82c13c7aebde396eb7152f
roflmaostc <fxw+git@mailbox.org>
2021-12-13 20:13:05 -0500

    Fix delta and theta functions

--- src/utils.jl
+++ src/utils.jl
@@ -1,100 +1,55 @@
 """
-    soft_theta(val, eps=0.1) = (val .> eps) ? 1.0 : ((val .< -eps) ? 0.0 : (1.0 .- cos((val .+ eps).*(pi/(2*eps))))./2) # to make the threshold differentiable
+    soft_theta(x::T, k=T(1)) where T
 
-this is a version of the theta function that uses a soft transition and is differentialble.
-
-Arguments:
-+ val: value to compare with zero
-+ eps: hardness of the step function (spanning from -eps to eps)
+This is a version of the theta function that uses a soft transition and is differentiable.
+It is implemented as `1 / (1+exp(-2 * k * x))`.
+A larger `k` makes the edge harder.
 """
-soft_theta(val, eps=0.01) = (val .> eps) ? 1.0 : ((val .< -eps) ? 0.0 : (1.0 .- cos.((val .+ eps).*(pi/(2*eps))))./2.0) # to make the threshold differentiable
-
-"""
-    soft_delta(val, eps=0.1) = (val .> eps) ? 1.0 : ((val .< -eps) ? 0.0 : (1.0 .- cos((val .+ eps).*(pi/(2*eps))))./2) # to make the threshold differentiable
-
-this is a smooth version of the theta function that uses a soft peak and is differentialble.
-The sum is not normalized but the value at val is one.
-
-Arguments:
-val: value to compare with zero
-eps: hardness of the step function (spanning from -eps to eps)
-"""
-soft_delta(val, eps=0.01) = (abs2.(val) .> abs2.(eps)) ? 0.0 : (1.0 .+ cos.(val.*(pi/eps)))./2.0 # to make the threshold differentiable
+function soft_theta(x::T, k=1) where T
+    one(T) / (one(T) + exp(-T(2)*k*x)) 
+end
 
 """
-    exp_decay(t,τ, eps=0.1) 
+    soft_theta_pw(x::T, ϵ=T(0.01)) where T
 
-an exponential decay starting at zero and using a soft threshold.
-Note that this function can be applied to multiple decay times τ simulataneously, yielding multiple decays stacked along the second dimension
+A different version of the soft theta. Uses a `cos` instead of `exp` functions.
+Furthermore, is piecewise defined.
+A smaller `e` makes the edge harder.
 """
-exp_decay(t,τ, eps=0.01) = soft_theta.(t,eps) .* exp.( - (t ./ transpose(τ)))
-
-
-"""
-    multi_exp_decay(t,amps, τs, eps=0.1) 
-
-a sum of exponential decays starting at t==zero and using a soft threshold.
-
-Arguments:
-+ t: time series to apply this to
-+ amps: individual amplitudes as a vector
-+ τs : individual lifetimes as a vector
-+ eps: width of the soft edge
-"""
-multi_exp_decay(t, amps, τs, eps=0.01) = sum(transpose(amps).*exp_decay(t, τs, eps), dims=2)[:,1] 
-
-
-
-
-
-
-
-
-"""
-    pack(myTuple::Tuple, do_fit)
-
-This packs a tuple of values into a vector which is normalized per direction and returns an unpack function which reverts this.
-This tool is useful for fit routines.
-
-returns the packed tuples (with a position being true in do_fit) as a vector and the unpack algorithm as a closure.
-"""
-function pack(myTuple::Tuple, do_fit; rel_scale=nothing, dtype=Float64)
-    scales = []
-    vec = dtype[]
-    lengths = Int[]
-    for (t,f) in zip(myTuple,do_fit)
-        if !isnothing(rel_scale)
-            scale = sum(t)/length(t) .* rel_scale
-        else
-            scale = 1.0
-        end
-        if f
-            push!(scales, scale)
-            vec = cat(vec, t ./ scale, dims=1)
-            push!(lengths, length(t))
-        else # save the constant
-            push!(scales, t)
-            push!(lengths, length(t))
-        end
+function soft_theta_pw(x::T, ϵ=T(0.01)) where T
+    if x .> ϵ
+        one(T) 
+    elseif x .< -ϵ
+        zero(T) 
+    else
+        (one(T) - cos((x + ϵ) * (T(π) / (T(2)*ϵ)))) / T(2) 
     end
-
-    function unpack(vec)
-        res = ()
-        pos = 1
-        for (l,s,f) in zip(lengths,scales, do_fit)
-            if f
-                vals = vec[pos:pos+l-1] .* s
-                pos += l
-            else
-                vals = s
-            end
-            res = (res..., vals)
-        end
-        return res
-    end
-    
-    return Vector{dtype}(vec), unpack # Vector{Int}(lengths), Vector{Float64}(scales) 
 end
 
 
+"""
+    soft_delta(x::T, k=T(1)) where T
 
+This is a smooth version of the delta function that uses a soft peak and is differentiable.
+Based on a Gauss function.
+A larger `k` makes the edge harder.
+"""
+function soft_delta(x::T, k=T(1)) where T
+    one(T) / (k * √(T(π))) * exp(-abs2(x / k))
+end
+
+
+"""
+    soft_delta_pw(x::T, ϵ=T(1)) where T
+
+This is a smooth version of the delta function that uses a soft peak and is differentiable.
+Based on a Gauss function.
+A larger `k` makes the edge harder.
+"""
+function soft_delta_pw(x::T, ϵ=T(0.01)) where T
+    if abs(x) > abs(ϵ)
+        zero(T)
+    else
+        (one(T) + cos(x * (T(π) / ϵ))) / 2
+    end
+end
--- test/utils.jl
+++ test/utils.jl
@@ -1,17 +1,31 @@
 @testset "Utils" begin
 
     @testset "Test soft_theta" begin 
-        @test soft_theta(0.01) == 1.0
-        @test soft_theta(-0.01) == 0.0
-        @test soft_theta(-0.0) ≈ 0.5
-        @test soft_theta(1/3.0, 1.0) ≈ 0.75
+        x = -1:0.1:1
+        @test eltype(MicroscopyTools.soft_theta.(Float32.(x))) == Float32
+        @test MicroscopyTools.soft_theta.(x, Ref(13.1)) ≈ 1 ./ (1 .+exp.(-2 .*13.1 .*x))
+        @test MicroscopyTools.soft_theta_pw(0.01) == 1.0
+        @test typeof(MicroscopyTools.soft_theta_pw(0.01f0)) == Float32
+        @test MicroscopyTools.soft_theta_pw(-0.01) == 0.0
+        @test MicroscopyTools.soft_theta_pw(-0.0) ≈ 0.5
+        @test MicroscopyTools.soft_theta_pw(1/3.0, 1.0) ≈ 0.75
     end
     
     @testset "Test soft_delta" begin 
-        @test soft_delta(0.01) == 0.0
-        @test soft_delta(-0.01) == 0.0
-        @test soft_delta(-0.0) ≈ 1.0
-        @test soft_delta(1/3.0, 1.0) ≈ 0.75
+        
+        T = Float64
+        f(x, k) = one(T) / (k * √(T(π))) * exp(-abs2(x / k))
+
+        x = -1:0.1:1
+        @test eltype(MicroscopyTools.soft_delta.(Float32.(x))) == Float32
+        @test MicroscopyTools.soft_delta.(x, Ref(13.1)) ≈ f.(x, 13.1) 
+
+
+        @test typeof(MicroscopyTools.soft_delta_pw(0.01f0)) == Float32
+        @test MicroscopyTools.soft_delta_pw(0.01f0) == 0.0
+        @test MicroscopyTools.soft_delta_pw(-0.01) == 0.0
+        @test MicroscopyTools.soft_delta_pw(-0.0) ≈ 1.0
+        @test MicroscopyTools.soft_delta_pw(1/3.0, 1.0) ≈ 0.75
     end
 
 end
