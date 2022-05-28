56f72fd24966c5f6ca63716a75c75d05cc5f5409
RainerHeintzmann <heintzmann@gmail.com>
2022-05-28 15:01:09 +0200

    more tools

--- src/MicroscopyTools.jl
+++ src/MicroscopyTools.jl
@@ -5,11 +5,14 @@ using FFTW
 using NDTools
 using Statistics
 using IndexFunArrays
+using Images # find_local_maxima
+using NearestNeighbors # to discard them in PSF extraction
+using InverseModeling # for gauss_fit
+using FourierTools # for shift
 
 include("binning.jl")
 include("calculation_tools.jl")
 include("utils.jl")
 include("grid_generation.jl")
-
 
 end
